ccb10f2cc8777a248c81cd33486d78c18bc52947
otwoma <otwomadennis365@gmail.com>
2021-12-09 10:47:08 +0300

    defined the skeleton structure of the frontend and started connecting the frontend and the backend

---
+++ client/src/api/index.js
@@ -0,0 +1,7 @@
+import axios from 'axios';
+
+//we will use axios to make api calls
+const url = 'http://localhost:5000/posts'; //this is the url pointing to our backend route
+
+// remeber http://localhost:5000/posts returns all the posts that we have in the database.
+export const fetchPosts = () => axios.get(url);
